8e4e0ca4b8092b43d66dae4112ad7e8c905d7e90
Gavin Coulson <gavincoulson@Gavins-MacBook-Pro.local>
2024-10-24 17:56:30 -0600

    updates

---
+++ absolute.py
@@ -0,0 +1,14 @@
+import os
+
+# Define the base directory (absolute path to the project folder)
+base_dir = '/Users/gavincoulson/PythonAutomationProject1/pythonProject'
+
+# Construct the absolute path to the dad_jokes.txt file
+file_path = os.path.join(base_dir, 'absolute_v_relative', 'dad_jokes.txt')
+
+# Open and read the file
+with open(file_path, 'r') as file:
+    dad_jokes = file.read()
+
+# Print the content of the file
+print(dad_jokes)
---
+++ relative.py
@@ -0,0 +1,7 @@
+import os
+
+# Create the path dynamically
+path = os.path.join('absolute_v_relative', 'dad_jokes.txt')
+
+with open(path, 'r') as file:
+    print(file.read())

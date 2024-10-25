5e52f118e4c3b80e088facb0c7f0b2aeeeb2b552
Gavin Coulson <gavincoulson@Gavins-MacBook-Pro.local>
2024-10-24 18:34:05 -0600

    getting file types

--- iterable_filepaths.py
+++ iterable_filepaths.py
@@ -1,7 +1,18 @@
 from pathlib import Path
 
+import magic
+
 path = Path.home() / "PythonAutomationProject1" / "pythonProject"
 
+magic_instance = magic.Magic(mime=True)
 # A pretty cool way of iterating over a directory
 for item in path.iterdir():
-    print(item)
+    if item.is_file():
+        print(item.name, "is a file")
+        print(magic_instance.from_file(item))
+        # accessing the file type by what it ends with, doesn't actually look at what type the file is
+        print(item.suffix)
+    elif item.is_dir():
+        print(item.name, "is a directory")
+    else:
+        print(item, "is not a file nor a directory")

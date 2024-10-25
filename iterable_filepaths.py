ee8f506d83a6616ddce093e8372ff5139078cd77
Gavin Coulson <gavincoulson@Gavins-MacBook-Pro.local>
2024-10-24 18:39:51 -0600

    fun times with dates and all that other stuff

--- iterable_filepaths.py
+++ iterable_filepaths.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pathlib import Path
 
 import magic
@@ -11,8 +12,17 @@ for item in path.iterdir():
         print(item.name, "is a file")
         print(magic_instance.from_file(item))
         # accessing the file type by what it ends with, doesn't actually look at what type the file is
+        stats = item.stat()
+        # get the size and last time it was modified
+        print("Size: ", stats.st_size)
+        # Goes from January 3rd, 1970
+        print("Last time modified: ", stats.st_mtime)
+        print(
+            "Last time modified human readable", datetime.fromtimestamp(stats.st_mtime)
+        )
         print(item.suffix)
+
     elif item.is_dir():
         print(item.name, "is a directory")
-    else:
-        print(item, "is not a file nor a directory")
+    if "read" in item.name.lower():
+        print(item.name, "contains the word read")
